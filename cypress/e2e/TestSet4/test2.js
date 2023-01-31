f7e2bd9ecb4ec866233ff26fafc0afeb937b651b
Prashant Bhosale <bhosale.prashant09@gmail.com>
2023-01-31 12:55:57 +0530

    Updated ci

--- cypress/e2e/TestSet4/test2.js
+++ cypress/e2e/TestSet4/test2.js
@@ -7,7 +7,7 @@ describe("Qa Test", () => {
     () => {
       for (let i = 1; i < 2; i += 1) {
         cy.log('waiting for a min : '+i);
-        cy.wait(10000);
+        cy.wait(5000);
       }
     },
   );
